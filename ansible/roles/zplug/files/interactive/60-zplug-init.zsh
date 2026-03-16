c4828617c6b5f5bdb50e09186e4149975d85e4fd
timsu92 <33785401+timsu92@users.noreply.github.com>
2026-03-16 18:22:45 +0000

    fix(zplug): avoid stdio detection

--- ansible/roles/zplug/files/interactive/60-zplug-init.zsh
+++ ansible/roles/zplug/files/interactive/60-zplug-init.zsh
@@ -1,4 +1,5 @@
 source ~/.zplug/init.zsh
+
 zplug 'zplug/zplug',                                hook-build:'zplug --self-manage'
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-autosuggestions'
@@ -10,9 +11,16 @@ zplug 'timsu92/vimrc',                              use:'dummy.file', hook-build
 zplug 'timsu92/ee65b1285ae128ba91d88ce972c91a95',   from:gist, as:command, use:'mosh-ssh-bridge.bash'
 zplug 'timsu92/ba3c31cd48d5a384cf5844a1329779c1',   from:gist, as:command, use:'rs'
 
-if ! zplug check --verbose; then
-	printf "Install zplug ? [y/N]: "
-	if read -q; then
-		echo; zplug install
+# If ZPLUG_SKIP_PROMPT is set (by Ansible), skip the prompt and install directly.
+# Otherwise, prompt the user interactively (normal login behavior).
+if [[ -n "${ZPLUG_SKIP_PROMPT:-}" ]]; then
+	# (zplug check --verbose || zplug install) 2>&1 | grep -v 'pipe syntax is deprecated' || true
+	zplug check --verbose || zplug install
+else
+	if ! zplug check --verbose; then
+		printf "Install zplug ? [y/N]: "
+		if read -q; then
+			echo; zplug install
+		fi
 	fi
 fi
